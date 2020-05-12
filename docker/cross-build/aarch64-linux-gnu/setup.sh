da783d81cee72650027f94932422ffb34a039db0
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-12 19:48:26 -0400

    Update setup.sh

--- docker/cross-build/aarch64-linux-gnu/setup.sh
+++ docker/cross-build/aarch64-linux-gnu/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sudo apt install cmake ninja-build clang clang-format lldb lld gcc g++ vim cargo htop tmux git curl \
                  python3-distutils expect net-tools di tig wget \
                  libstdc++-8-dev-arm64-cross libstdc++6-arm64-cross libgcc1-arm64-cross libgcc-8-dev-arm64-cross
