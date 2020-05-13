3196ba60038854ea0a16bb981f11e7cd33f5b894
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-12 20:35:39 -0400

    Update setup.sh

--- docker/cross-build/aarch64-linux-gnu/setup.sh
+++ docker/cross-build/aarch64-linux-gnu/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sudo apt install cmake ninja-build clang clang-format lldb lld gcc g++ vim cargo htop tmux git curl \
-                 python3-distutils expect net-tools di tig wget \
-                 libstdc++-8-dev-arm64-cross libstdc++6-arm64-cross libgcc1-arm64-cross libgcc-8-dev-arm64-cross
+apt update
+apt install cmake ninja-build clang clang-format lldb lld gcc g++ vim cargo htop tmux git curl \
+            python3-distutils expect net-tools di tig wget \
+            libstdc++-8-dev-arm64-cross libstdc++6-arm64-cross libgcc1-arm64-cross libgcc-8-dev-arm64-cross
 
 cd
 git clone http://github.com/llvm/llvm-test-suite
