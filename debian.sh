a5ce2b3b4d2b374dd5f0aad8f180e70badfa7921
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-12-20 14:54:34 -0500

    debian.sh
    Just some one liners to get the ball rolling on a new machine/docker instance.

---
+++ debian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# essential dev tools
+sudo apt install cmake ninja-build clang clang-format clangd clang-tidy lldb lld vim tmux git
+
+# handy console tools
+sudo apt install ripgrep htop net-tools di tig
