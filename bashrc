7ccd4bb3d1016fe4d4e3d56a24c36b721938cf55
Puyan Lotfi <puyan@puyan.org>
2020-12-28 19:36:43 -0500

    setup script

--- bashrc
+++ bashrc
@@ -1,8 +1,6 @@
 stty -ctlecho
-export PATH=~/bin:~/.cargo/bin:~/Tools/clang+llvm/bin:.:./build/bin:$PATH
 export EDITOR=vim
 export VISUAL=$EDITOR
-export TERM=xterm-256color
 alias r=reset
 alias j=jobs
 
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+echo "Beware, this script will remove or modify your dotfiles."
+read
+
+cd `dirname "$0"`
+
+#####################
+## Setup gitconfig ##
+#####################
+rm -f ~/.gitconfig ~/.tmux.conf ~/.vim ~/.vimrc
+
+echo "Enter git email:"
+read
+git config --global user.email "$REPLY"
+
+echo "Enter git name:"
+read
+git config --global user.name "$REPLY"
+
+cat gitconfig >> ~/.gitconfig
+#####################
+
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/vim ~/.vim
+ln -s $(pwd)/vim/init.vim ~/.vimrc
+echo "source $(pwd)/bashrc" >> ~/.bashrc
