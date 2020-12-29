e8bcc47b464d05f4b86e9a71f24256efa2011f4d
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-12-28 21:01:19 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # essential dev tools
-sudo apt install cmake ninja-build clang clang-format clangd clang-tidy lldb lld vim tmux git
+sudo apt install -y cmake ninja-build clang clang-format clangd clang-tidy lldb lld vim tmux git
 
 # build deps
-sudo apt install python3-distutils 
+sudo apt install -y python3-distutils 
 
 # handy console tools
-sudo apt install ripgrep htop net-tools di tig expect wget curl
+sudo apt install -y ripgrep htop net-tools di tig expect wget curl
 
 # remoting etc
-sudo apt install barrier gparted openssh-server x11vnc
+sudo apt install -y barrier gparted openssh-server x11vnc
 
 # git/vscode auth WA for Kubuntu
-sudo apt install gnome-keyring
+sudo apt install -y gnome-keyring
 
 # rust + cargo
-sudo apt install cargo
+sudo apt install -y cargo
 
 # Docker (The Debian/Ubuntu Distributed One) and Virt-Manager+KVM+QEMU
-sudo apt install docker.io virt-manager
+sudo apt install -y docker.io virt-manager
 
 # For Formating nvme dev drives
-sudo apt install gparted
+sudo apt install -y gparted
 
 # Password Management
-sudo apt install keepassxc
+sudo apt install -y keepassxc
 
 # Add the following to /ets/fstab for nvme mount:
 # LABEL=FastBoy                             /mnt/FastBoy    ext4    defaults          0       0
