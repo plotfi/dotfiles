a42e968f498908a384af77bb4ffa481ecc640d7e
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-12-29 00:57:48 -0500

    Update install-lsp.sh

--- vim/plugins/install-lsp.sh
+++ vim/plugins/install-lsp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf LanguageClient-neovim
-git clone --depth 1 https://github.com/autozimu/LanguageClient-neovim.git
+git clone --depth 1 https://github.com/autozimu/LanguageClient-neovim
 cd LanguageClient-neovim
 bash install.sh
