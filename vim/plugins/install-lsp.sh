52d6521618fc5232ce951a48ee92cadf7754d6bc
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-12-29 00:50:30 -0500

    Update install-lsp.sh

--- vim/plugins/install-lsp.sh
+++ vim/plugins/install-lsp.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# rm -rf LanguageClient-neovim deoplete.nvim
-
-# Un-comment for code completion.
-# git clone https://github.com/Shougo/deoplete.nvim.git
-
+rm -rf LanguageClient-neovim
 git clone --depth 1 https://github.com/autozimu/LanguageClient-neovim.git
 cd LanguageClient-neovim
 bash install.sh
