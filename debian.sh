7b96eb79a8740bea79b3cb51e463c4644324eadf
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-12-30 22:10:12 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -3,7 +3,7 @@
 # Update + Requirements
 sudo apt update
 sudo apt -y dist-upgrade
-sudo apt install -y git gparted
+sudo apt install -y git gparted apt-transport-https
 
 # essential dev tools
 sudo apt install -y cmake ninja-build clang clang-format clangd clang-tidy lldb lld vim tmux git
@@ -37,6 +37,14 @@ sudo apt install -y gparted
 
 # Password Management
 sudo apt install -y keepassxc
+
+# Add Microsoft Linux Package Repository and install VSCode (Insiders):
+# https://docs.microsoft.com/en-us/windows-server/administration/linux-package-repository-for-microsoft-software
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-add-repository 'deb [arch=amd64] https://packages.microsoft.com/repos/code stable main'
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code-insiders
 
 # Add the following to /ets/fstab for nvme mount:
 # LABEL=FastBoy                             /mnt/FastBoy    ext4    defaults          0       0
